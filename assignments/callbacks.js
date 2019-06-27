6b53fa327fcae269c80d0874ac679a4664b812eb
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-06-27 17:05:48 -0400

    closures.js completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,5 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+console.log(`Create a higher order function and invoke the callback function to test your work.`);
+// You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -24,29 +25,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+console.log(`getLength passes the length of the array into the callback.`);
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  
 }
 
+console.log(`last passes the last item of the array into the callback.`);
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  
 }
 
+console.log(`sumNums adds two numbers (x, y) and passes the result to the callback.`);
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+ 
 }
 
+console.log(`multiplyNums multiplies two numbers and passes the result to the callback.`)
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  
 }
 
+console.log(`contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.`)
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  
 }
 
-/* STRETCH PROBLEM */
+console.log (`* STRETCH PROBLEM *`);
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,54 @@
-// ==== Challenge 1: Write your own closure ====
+console.log(`==== Challenge 1: Write your own closure ====`);
 // Write a simple closure of your own creation.  Keep it simple!
+console.log(`===Option1===`)
 
+const Greeting = name => {
+  console.log(`Good morning, ${name}`);
+
+  const frenchGreeting = () => {
+    console.log(`Bonjour, ${name}`);
+
+    const russianGreeting = () => {
+      console.log(`Privet, ${name}`);
+    };
+    russianGreeting(name);
+  };
+  frenchGreeting(name);
+};
+Greeting("Vlad");
+
+console.log(`===Option2===`);
+function person() {
+  let name = 'Vlad';
+  
+  return function displayName() {
+    console.log(name);
+  };
+}
+let vlad = person();
+vlad(); 
+
+
+ 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
-// ==== Challenge 2: Create a counter function ====
+console.log(`==== Challenge 2: Create a counter function ====`);
 const counter = () => {
+  let count = 0;
   // Return a function that when invoked increments and returns a counter variable.
+  return() => {
+    return ++count;
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
